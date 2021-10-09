a0aed70baee752888dca83c489c490636929fa76
Bunga Permata Hilias <bungaaaph@gmail.com>
2021-10-09 23:58:32 +0700

    Create DataDiri.java

---
+++ src/main/java/com/spboot/MotorTuaMuseum/models/DataDiri.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spboot.MotorTuaMuseum.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author bungaaaph
+ */
+@Entity
+@Table(name="data_diri")
+
+public class DataDiri {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(name="nama")
+    private String nama;
+    
+    @Column(name="tanggal_lahir")
+    private String tanggal_lahir;
+    
+    @Column(name="alamat")
+    private String alamat;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column (name="no_telp")
+    private int no_telp;
+    
+    public void setNama(String nama){
+    this.nama = nama;
+    }
+    
+    public String getName(){    
+        return nama;
+    }
+    
+    public void setTanggal_lahir(String tanggal_lahir){
+    this.tanggal_lahir = tanggal_lahir;
+    }
+    
+    public String getTanggal_lahir(){
+    return tanggal_lahir;
+    }
+    
+    public void setTAlamat(String alamat){
+    this.alamat = alamat;
+    }
+    
+    public String getAlamat(){
+    return alamat;
+    }
+    
+    public void setEmail(String email){
+    this.email = email;
+    }
+    
+    public String getEmail(){
+    return email;
+    }
+    
+    public void setNo_telp(int no_telp){
+    this.no_telp = no_telp;
+    }
+    
+    public int getNo_telp(){
+    return no_telp;
+    }
+    
+    
+    
+}
